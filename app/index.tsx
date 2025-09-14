d7e0092a2f854a5aae831f41b953741c8f730abe
sumonpaul55 <sumonpaul.web@gmail.com>
2025-09-14 18:43:17 +0600

    checking the native wind woking now. changing the path.

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text>Welcome to mobile software</Text>
+      <Text className="text-4xl font-bold text-red-500">Hello everyone</Text>
+      <Text className="text-2xl font-bold text-indigo-500">this is Sumon Chandra Paul</Text>
     </View>
   );
 }
