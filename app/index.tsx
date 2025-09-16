7d401d413b894421fbf966b43f21a87dc8db5c1d
sumonpaul55 <sumonpaul.web@gmail.com>
2025-09-16 13:03:29 +0600

    native wind configer done with theme configer base color as well. now its ready for go

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     <View className="flex-1 py-10 items-center">
       <Text className="font-semibold text-accesnt text-5xl">Welcome!</Text>
       <Text>I&apos;m Apk Software for Mobile (android and ios)</Text>
-      <Text className="text-xl mt-5 font-bold text-secondary">Hello everyone</Text>
+      <Text className="text-xl mt-5 font-bold text-dark-200">Hello everyone</Text>
       <Text className="mt-10 font-bold px-4 text-indigo-800">this is Sumon Chandra Paul from Mobile Application</Text>
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
       colors: {
         primary: "#030014",
         secondary: "#151312",
+        light: {
+          100: "#d6c6ff",
+          200: "#a8b5db",
+          300: "#9ca4ab"
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
         accesnt: "#ab5bdd"
       }
     },
