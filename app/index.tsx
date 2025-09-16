7c507b3aca5a7a420f6d529eef240cfa53421cfd
sumonpaul55 <sumonpaul.web@gmail.com>
2025-09-16 12:29:32 +0600

    tailwnd or native wind configer for all pages in the app or oter directory

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     <View className="flex-1 justify-center items-center">
       <Text>Welcome to mobile software</Text>
       <Text className="text-4xl font-bold text-red-500">Hello everyone</Text>
-      <Text className="text-2xl font-bold text-indigo-500">this is Sumon Chandra Paul</Text>
+      <Text className="text-2xl font-bold px-4 text-indigo-800">this is Sumon Chandra Paul from Mobile Application</Text>
     </View>
   );
 }
