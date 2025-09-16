415f4912561ef4d5ce1ab1180c22ede7e0a24806
sumonpaul55 <sumonpaul.web@gmail.com>
2025-09-16 13:30:00 +0600

    navigate and a route created

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 py-10 items-center">
       <Text className="font-semibold text-accesnt text-5xl">Welcome!</Text>
-      <Text>I&apos;m Apk Software for Mobile (android and ios)</Text>
+      <Text>I&apos;m Software for Mobile (android and ios)</Text>
       <Text className="text-xl mt-5 font-bold text-dark-200">Hello everyone</Text>
       <Text className="mt-10 font-bold px-4 text-indigo-800">this is Sumon Chandra Paul from Mobile Application</Text>
+      <Link href="/onboard" className="mt-4 bg-primary text-white px-3 py-2 rounded-lg">
+        <Text>Onborading</Text>
+      </Link>
     </View>
   );
 }
