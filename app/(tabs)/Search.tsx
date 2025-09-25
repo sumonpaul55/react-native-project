d2b4f277e76ca0bcc1fcd2a4ffaaeba0cd646b37
sumonpaul55 <sumonpaul.web@gmail.com>
2025-09-26 01:13:04 +0600

    search page moviie fetched and displyed

--- app/(tabs)/Search.tsx
+++ app/(tabs)/Search.tsx
@@ -1,12 +1,37 @@
+import MovieCart from '@/components/MovieCart'
+import { images } from '@/constants/images'
+import { fetchMovie } from '@/services/api'
+import useFetch from '@/services/useFetch'
+import { useRouter } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Image, Text, View } from 'react-native'
 
-const search = () => {
+const Search = () => {
+  const router = useRouter()
+  const { data: movies, loading: movieLoding, error: moveisError } = useFetch(() => fetchMovie({ query: "" }));
+
   return (
-   <View className='p-4'>
-         <Text className='text-3xl font-semibold'>Search page</Text>
-       </View>
+    <View className='flex-1 items-center pt-20 bg-primary'>
+      <Image source={images.bg} className="w-full h-full absolute z-0" />
+      <Text className='text-white text-5xl font-bold'>Search</Text>
+
+      <FlatList data={movies?.results}
+        renderItem={({ item }) => (
+          <MovieCart {...item} />
+        )}
+        keyExtractor={(item) => item?.id.toString()}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "flex-start",
+          gap: 20,
+          marginBottom: 10,
+          paddingRight: 5
+        }}
+        className="mb-2 pb-32 mt-5"
+        scrollEnabled={true}
+      />
+    </View>
   )
 }
 
-export default search
+export default Search
